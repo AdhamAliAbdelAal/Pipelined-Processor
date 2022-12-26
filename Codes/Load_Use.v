8550a30d96fcaef3fe886b09048f7747bf59d5a2
MohamedWw <mohamedwalid1960@gmail.com>
2022-12-26 21:01:33 +0200

    Fixed Load Use Case

--- Codes/Load_Use.v
+++ Codes/Load_Use.v
@@ -1,11 +1,12 @@
 /*MR|SP*/
 module Load_Use_Case(
-    MR,EXEC_Dst, DEC_Src1, DEC_Src2, Load_Use
+    MR,EXEC_Dst, DEC_Src1, DEC_Src2, Keep_Fetched_Instruction, Keep_PC, Flush_MUX_Selector
 );
     input MR;
-    input [2:0] EXEC_Dst, DEC_Src;
-    output Load_Use;
+    input [2:0] EXEC_Dst, DEC_Src1, DEC_Src2;
+    output Keep_Fetched_Instruction, Keep_PC, Flush_MUX_Selector;
 
-    assign Load_Use = (MR==1'b1 && (EXEC_Dst===DEC_Src1 || EXEC_Dst===DEC_Src2))? 1'b1: 1'b0;
+    assign {Flush_MUX_Selector, Keep_PC, Keep_Fetched_Instruction} = 
+        (MR==1'b1 && (EXEC_Dst===DEC_Src1 || EXEC_Dst===DEC_Src2))? 3'd7: 3'd0;
 
 endmodule
