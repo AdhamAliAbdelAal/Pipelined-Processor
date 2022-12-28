feb700991131726eb6f6ee2e59e16b9c8e714441
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2022-12-28 16:14:56 +0200

    Update Program_Counter.v

--- Codes/Program_Counter.v
+++ Codes/Program_Counter.v
@@ -1,16 +1,29 @@
-module Program_Counter(reset ,clk, PC_Out, stall);
-input clk, reset, stall;
+module Program_Counter(reset ,clk, PC_Out, stall,INT,JMP,JWSP,accPC,Dst);
+input clk, reset, stall,INT,JMP,JWSP;
+input [31:0] accPC,Dst;
 output reg[31:0]PC_Out;
 
 always @(posedge clk)
 begin
-if(reset ==1)
+if(INT===1'b1)
+begin
+    PC_Out=32'd0;
+end
+else if(reset ==1)
 begin
 PC_Out = {{26{1'b0}},6'b10_0000};
 end
 else if (stall===1'b1)
 begin
   PC_Out=PC_Out;
+end
+else if(JMP==1'b1)
+begin
+    PC_Out = Dst;
+end
+else if(JWSP==1'b1)
+begin
+    PC_Out = accPC;
 end
 else if (PC_Out<32'd50 && stall===1'b0)begin
 PC_Out=PC_Out + 1;
